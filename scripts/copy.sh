8f4effde59d7be1679035a0eaec0fa6fad4908ea
Fabien Bourgeois <fbourgeois@yaltik.com>
2014-07-12 08:55:37 +0200

    0.3 branching, dependencies updated

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -8,4 +8,5 @@ cp -r $SRC/open-sans-fontface/fonts $DEST/ &&
 cp $SRC/open-sans-fontface/open-sans.css $DEST/ &&
 cp $SRC/notify.js/notify.js $DEST/ &&
 cp $SRC/semantic-ui/build/packaged/css/semantic.css $DEST/
-# TODO :mithril, pouchdb
+cp $SRC/mithril/mithril.js $DEST/
+# TODO : PouchDb
